9a13ee94fb5ef7b14f1aad96391492efc5cd9d67
Auchendale <kpnparagon@sky.com>
2024-11-04 20:34:15 +0000

    4: Articles and a tad bit of CSS

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,17 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import Header from './components/Header'
+import Homepage from './components/Homepage'
+import {Routes, Route} from "react-router-dom"
 
 function App() {
 
   return (
     <>
-      
+      <Header/>
+      <Routes>
+        <Route path="/" element={<Homepage/>}/>
+      </Routes>
     </>
   )
 }
---
+++ src/components/Header.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import { Link } from "react-router-dom"
+import logo from '../assets/kpedditLogo1.png'
+
+export default function Header (){
+
+    return (
+        <>
+        <div className="header-grid">
+            <div className="header-grid-item-logo">            
+                <img id="kpedditLogo" src={logo}></img>
+            </div>
+            <div className="header-grid-item-title">
+                <h1>kpeddit</h1>
+            </div>
+            <div className="header-grid-item-topic">
+                <h2></h2>
+            </div>
+        </div>
+        </>
+    )
+}
